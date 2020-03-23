53d5f1bc50939c4ade5a234a03220cda38a351e0
vztpv <vztpv@naver.com>
2020-03-23 15:21:16 +0900

    ~3,4,1 ex4?

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import MyCommponent from './ex3/MyComponent.js';
+import Counter from './ex4/Counter.js'
 
-function App() {
+const App = () => {
   return (
     <div className="React">
       <MyCommponent name='hello' favoriteNumber={33}> <br /> TEST <br />
       </MyCommponent>
+      <Counter />
     </div >
   );
 }
---
+++ hello-react/src/ex4/Counter.js
@@ -0,0 +1,39 @@
+
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        number: 0,
+        fixedNumber: 0
+    };
+
+    render() {
+        const { number, fixedNumber } = this.state;
+        return (
+            <div>
+                <h1>{number}</h1>
+                <h2>{fixedNumber}</h2>
+                <button
+                    onClick={() => {
+                        this.setState(prevState => {
+                            return {
+                                number: prevState.number + 1
+                            };
+                        });
+
+                        this.setState(prevState => {
+                            return {
+                                number: prevState.number + 1
+                            };
+                        });
+                    }
+                    }
+                >
+                    +1
+                </button>
+            </div >
+        );
+    }
+}
+
+export default Counter;
