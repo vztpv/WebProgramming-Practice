3cef42ad8236bfac8b65da9c312c65b86f2dfa58
vztpv <vztpv@naver.com>
2020-03-29 13:42:51 +0900

    ~6.4.3 ex11?

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import ScrollBox from './ex10/ScrollBox.js';
+import IterationSample from './ex11/IterationSample.js'
 
 class App extends Component {
   render() {
     return (
       <div className="React" >
-        <ScrollBox ref={(ref) => this.ScrollBox = ref} />
-        <button onClick={() => this.ScrollBox.scrollToBottom()}>
-          to bottom
-      </button>
+        <IterationSample></IterationSample>
       </div >
     );
   }
